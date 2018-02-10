33f4c730316c89cfdfc71847c435815a9c4f565e
Alex Tsirozidis <saltypickles@yahoo.com>
2018-02-10 01:26:22 -0600

    corrected hlp

--- bin/lidless-eye.js
+++ bin/lidless-eye.js
@@ -6,6 +6,7 @@ const packgjson = require('../package.json');
 
 
 program
+  .command('lidless')
   .version(packgjson.version)
   .description(packgjson.description)
   .option('-f, --file', 'The .yml file containing what to monitor and how to respond to changes')
